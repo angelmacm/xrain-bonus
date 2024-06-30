416d2d82f4a348f92d599fdb4cdb816470cc67cd
ammacapagal <73205874+angelmacm@users.noreply.github.com>
2024-06-30 11:52:41 +0800

    Setup logging module

---
+++ src/utils/logging.py
@@ -0,0 +1,22 @@
+import logging
+
+# Create a logger
+loggingInstance = logging.getLogger(__name__)
+loggingInstance.setLevel(logging.DEBUG)  # Set the desired logging level
+
+# Create a file handler
+file_handler = logging.FileHandler('xrpl-claims.log')
+file_handler.setLevel(logging.DEBUG)  # Set the desired logging level for the file
+
+# Create a console handler
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)  # Set the desired logging level for the console
+
+# Create a formatter and set it for both handlers
+formatter = logging.Formatter('[%(asctime)s] %(levelname)s: %(module)s\\%(funcName)s    %(message)s')
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+
+# Add the handlers to the logger
+loggingInstance.addHandler(file_handler)
+loggingInstance.addHandler(console_handler)
