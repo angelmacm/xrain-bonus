2f2134d5234b65b14ed66ae3de9b3ab368b2db1b
ammacapagal <73205874+angelmacm@users.noreply.github.com>
2024-07-09 22:56:03 +0800

    update code to match xrpl current commands

--- src/utils/xrplCommands.py
+++ src/utils/xrplCommands.py
@@ -2,6 +2,7 @@ from xrpl.asyncio.clients import AsyncWebsocketClient
 from xrpl.wallet import Wallet
 from xrpl.asyncio.account import get_balance, get_next_valid_seq_number
 from xrpl.asyncio.transaction import autofill_and_sign, submit_and_wait, autofill, sign, submit
+from xrpl.asyncio.ledger import get_fee, get_latest_validated_ledger_sequence
 from xrpl.models.transactions import Payment, Memo
 from xrpl.utils import xrp_to_drops
 from xrpl.models.requests.account_lines import AccountLines
@@ -175,8 +176,8 @@ class XRPClient:
         try:
             loggingInstance.info(f"Getting current ledger") if self.verbose else None
             # Get the current ledger index
-            ledgerResponse = await client.request({"command": "ledger_current"})
-            ledgerIndex = ledgerResponse.result["ledger_current_index"]
+            # ledgerResponse = await client.request({"command": "ledger_current"})
+            ledgerIndex = await get_latest_validated_ledger_sequence(client)
             
             loggingInstance.info(f"Current ledger: {ledgerIndex}") if self.verbose else None
             loggingInstance.info(f"Getting next valid sequence number") if self.verbose else None
@@ -187,7 +188,7 @@ class XRPClient:
             loggingInstance.info(f"Next valid sequence number: {sequence}") if self.verbose else None
 
             # Manually fill in the required fields
-            transaction.fee = "15"  # Set a reasonable fee in drops
+            transaction.fee = await get_fee(client)  # Set a reasonable fee in drops
             transaction.sequence = sequence
             transaction.last_ledger_sequence = ledgerIndex + 10  # Set last ledger sequence to a few ledgers ahead
 
