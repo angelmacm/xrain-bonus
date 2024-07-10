44cfc1da813055b936e83fbab790df077dc477f7
ammacapagal <73205874+angelmacm@users.noreply.github.com>
2024-07-10 17:12:17 +0800

    create claimQuotes model

---
+++ src/database/models/claimQuotes.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, VARCHAR
+from sqlalchemy.orm import column_property
+from sqlalchemy.ext.declarative import declarative_base
+
+# Define the base class
+Base = declarative_base()
+
+class ClaimQuotes(Base):
+    __tablename__ = 'claimQuotes'
+
+    quoteId = Column(int, index=True)
+    nftGroupName = column_property(Column('NFTGroupName', VARCHAR))
+    taxonId = Column(Integer)
+    description = Column(VARCHAR)
